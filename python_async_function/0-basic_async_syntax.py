61ac9e4fdb2c6575d78d8ae9de5a242389036083
CAMILO <cestrad5@gmail.com>
2023-08-28 13:19:11 -0500

    Setting folder and files

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """takes in an integer argument (max_delay, with a
+     default value of 10) named wait_random that waits
+     for a random delay between 0 and max_delay
+     (included and float value) seconds and eventually
+     returns it."""
+    delay = max_delay * random.random()
+    await asyncio.sleep(delay)
+    return delay
