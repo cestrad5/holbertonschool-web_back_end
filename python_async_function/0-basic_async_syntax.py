73112f8a6752f477f8f523f1a405376c118777c5
CAMILO <cestrad5@gmail.com>
2023-08-28 14:49:55 -0500

    Task 0 solved using random.uniform

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,6 +10,6 @@ async def wait_random(max_delay: int = 10) -> float:
      for a random delay between 0 and max_delay
      (included and float value) seconds and eventually
      returns it."""
-    delay = max_delay * random.random()
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
