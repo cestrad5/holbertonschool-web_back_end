e211247a1a47d831799ba865e79ed4eacfd98f32
CAMILO <cestrad5@gmail.com>
2023-08-28 20:21:24 -0500

    Task 1 solved

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,11 +13,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Async routine that spawns wait_random n times
     with the specified max_delay."""
 
-    # create a list of delays
     delays = [wait_random(max_delay) for i in range(n)]
-
-    # create a list of the order in which the delays were completed
+    
     completed_delays = [await delay for delay in asyncio.as_completed(delays)]
 
-    # return the list of completed delays
     return completed_delays
