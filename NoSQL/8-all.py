42728c84ba0ee918d50b44f399e065e02e09c702
CAMILO <cestrad5@gmail.com>
2023-09-07 03:16:55 -0500

    Task solved

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """list all mongo collection"""
+    return list(mongo_collection.find())
