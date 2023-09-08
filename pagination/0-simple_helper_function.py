8d26a4f3659f4746aaeccd55232c6db500469a36
CAMILO <cestrad5@gmail.com>
2023-09-08 18:44:20 -0500

    task solved try24 pycodestyle problem

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-"""Write a function named index_range that takes two integer arguments page and page_size"""
+"""Write a function named index_range that takes
+two integer arguments page and page_size"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """index range
+
+    Args:
+        page (int): Number of page
+        page_size (int): Total number of page
+
+    Returns:
+        tuple[int, int]: return in a list for those particular
+                         pagination parameters
     """
     # Calculate the start index
     start_index = (page - 1) * page_size
