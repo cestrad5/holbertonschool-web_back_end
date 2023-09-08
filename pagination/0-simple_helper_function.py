4a6765941104dc8f03e0aad7a49c70f04951ecdd
CAMILO <cestrad5@gmail.com>
2023-09-08 18:37:27 -0500

    task solved try23

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+"""Write a function named index_range that takes two integer arguments page and page_size"""
+from typing import Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """index range
+    """
+    # Calculate the start index
+    start_index = (page - 1) * page_size
+    # Calculate the end index
+    end_index = start_index + page_size
+    return (start_index, end_index)
--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,1 +1,91 @@
+#!/usr/bin/env python3
+"""
+2. Hypermedia pagination
+"""
+import csv
+import math
+from typing import List, Dict
+from typing import Tuple
 
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self._data = []
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get_page
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            List[List]: list of pages
+        """
+        assert isinstance(page, int) and\
+            page > 0, "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) \
+            and page_size > 0, "Page size must be an integer greater than 0"
+
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(dataset) or start_index < 0:
+            return []
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Function hypermedia pagination
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            Dict: Dictionnary of data and all information
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """index range
+
+    Args:
+        page (int): Number of page
+        page_size (int): Total number of page
+
+    Returns:
+        tuple[int, int]: return in a list for those particular
+                         pagination parameters
+    """
+    # Calculate the start index
+    start_index = (page - 1) * page_size
+    # Calculate the end index
+    end_index = start_index + page_size
+    return (start_index, end_index)
