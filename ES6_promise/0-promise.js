9fc63bfa1e69008e30cd1aecd30ff86aadb0e89e
CAMILO <cestrad5@gmail.com>
2023-10-06 10:01:58 -0500

    Task 0 try 2

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
   return new Promise((resolve) => {
-    // Simulate an asynchronous API response
-    setTimeout(() => {
-      resolve('API response received');
-    }, 2000); // Simulating a 2-second delay
+    resolve();
   });
 }
-
-export default getResponseFromAPI;
