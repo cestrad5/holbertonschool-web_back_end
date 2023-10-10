efc67e8a3881ea512a2dda3b008b3dfff9e2f65b
CAMILO <cestrad5@gmail.com>
2023-10-10 10:24:10 -0500

    Test v7

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,12 @@
 // Return a Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      if (resolve) {
-        resolve('Working OK');
-      } else {
-        reject(Error('Not working'));
-      }
-    });
-  }
-  
-  export default getResponseFromAPI;
-      
+  return new Promise((resolve, reject) => {
+    if (resolve) {
+      resolve();
+    } else {
+      reject(Error());
+    }
+  });
+}
+
+export default getResponseFromAPI;
